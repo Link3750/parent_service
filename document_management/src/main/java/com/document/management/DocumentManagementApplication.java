a31eb3d38ca8b5c799802be8833b43e5a1fe4bee
TanYuan <Link3750@aliyun.com>
2022-09-28 13:22:12 +0800

    文档管理部分

---
+++ document_management/src/main/java/com/document/management/DocumentManagementApplication.java
@@ -0,0 +1,16 @@
+package com.document.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class DocumentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentManagementApplication.class, args);
+    }
+
+}
